c68ab100a3684c79ab86b67ca6048d2afa066a03
nareshmarriiit <109891675+nareshmarriiit@users.noreply.github.com>
2022-07-24 17:35:22 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+def main():
+    number1 = st.number_input('Insert NUmber1')
+    st.write('The current number is ', number1)
+    number2 = st.number_input('Insert NUmber2')
+    st.write('The current number is ', number2)
+    subtraction=number2-number1
+    st.write('The current number is ', subtraction)
+    
+if __name__ == "__main__":
+    main()

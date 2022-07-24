26ef68d6236f97c0d1cea2f71f9a60cd81dfff9e
nareshmarriiit <109891675+nareshmarriiit@users.noreply.github.com>
2022-07-24 17:51:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import streamlit as st
 def main():
-    number1 = st.number_input('Insert NUmber1')
-    st.write('The current number is ', number1)
-    number2 = st.number_input('Insert NUmber2')
-    st.write('The current number is ', number2)
+    number1 = st.number_input('Insert Number1')
+    number2 = st.number_input('Insert Number2')
     subtraction=number2-number1
-    st.write('The current number is ', subtraction)
+    st.write('The subtraction of above numbers is  Number2-Number1 ', subtraction)
     
 if __name__ == "__main__":
     main()
